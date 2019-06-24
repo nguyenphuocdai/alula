ff51097ff27c4f5e0305fc72585bb7aef0c45bc2
dainguyenAlttekglobal <dai.nguyen@alttekglobal.com>
2019-06-24 17:22:56 +0700

    - Add icon cart, compare to home page public

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,14 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  HostListener,
+  Inject,
+  OnDestroy
+} from "@angular/core";
+import { DOCUMENT } from "@angular/common";
+import { Subscription } from "rxjs";
+import { CartService } from "../core/_services/cart.service";
+import { ProductSerivce } from "../core/_services/product.service";
 
 @Component({
-  selector: 'app-pages',
-  templateUrl: './pages.component.html'
+  selector: "app-pages",
+  templateUrl: "./pages.component.html"
 })
-export class PagesComponent implements OnInit {
+export class PagesComponent implements OnInit, OnDestroy {
+  isVisible: boolean = false;
+  cartSubscription: Subscription;
+  compareSubscription: Subscription;
+  cartProducts = [];
+  compareProducts = [];
 
-  constructor() { }
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private cartService: CartService,
+    private productService: ProductSerivce
+  ) {}
+  @HostListener("window:scroll", ["$event"])
+  onScroll(event) {
+    let number =
+      window.pageYOffset ||
+      this.document.documentElement.scrollTop ||
+      this.document.body.scrollTop ||
+      0;
 
+    this.isVisible = number >= 150 ? true : false;
+  }
   ngOnInit() {
+    this.cartSubscription = this.cartService.currentCart.subscribe(products => {
+      this.cartProducts = products;
+    });
+
+    this.compareSubscription = this.productService.currentCompareProducts.subscribe(
+      arrCompares => {
+        this.compareProducts = arrCompares;
+      }
+    );
+  }
+  ngOnDestroy(): void {
+    this.cartSubscription.unsubscribe();
+    this.compareSubscription.unsubscribe();
   }
 
+  toTop(){
+    window.scrollTo(0, 0);
+  }
 }
